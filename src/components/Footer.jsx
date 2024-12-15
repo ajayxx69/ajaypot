65bd004e5520ff724def1edd03779b6b5ca77c81
ajayxx69 <163808546+ajayxx69@users.noreply.github.com>
2024-12-15 22:29:04 +0530

    Initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -88,7 +88,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Mayur Hegde</Logo>
+        <Logo>Molaka ajay kumar</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
@@ -103,7 +103,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2024 Mayur Hegde. All rights reserved.
+          &copy; 2024 Molaka ajay kumar. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
